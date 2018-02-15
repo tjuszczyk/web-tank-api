8bb2584a06823311377c1acd614159befb53b89b
Emma Richards and Tomasz Juszczyk <pair+emma.richards+tomasz.juszczyk@sky.uk>
2018-02-15 08:47:31 +0000

    [TANKS-1|service] Setup a API service with spring-boot

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # web-tank-controller-api
 Agile University - Building Web Application course - tank controller API
+
+## Run a service locally:
+
+```bash
+./gradlew clean build && java -jar tank-controller-api/build/libs/tank-controller-api*.jar
+```
+ To test is server is up:
+ 
+ ```bash
+curl localhost:8080/health
+```
---
+++ tank-controller-api/src/main/java/pl/tj/tanks/TanksApiConfiguration.java
@@ -0,0 +1,13 @@
+package pl.tj.tanks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanksApiConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TanksApiConfiguration.class, args);
+    }
+
+}
